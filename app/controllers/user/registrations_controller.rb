bc1b91ffca01712b93daa0636db9e739c6241281
aqueza2 <aqueza2@gmail.com>
2017-05-06 17:02:04 -0500

    add back role to link in index page

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,9 +1,21 @@
 class User::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+   def new
+     p "___________"
+     p params[:role]
+     p "__________**********************************_"
+     @role = params[:role]
+     params[:role] = "garden"
+     build_resource({})
+     yield resource if block_given?
+     respond_with resource
+   end
+
    protected
 
    def configure_permitted_parameters
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :email, :zip_code, :category, :work_description, :password])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :email, :zip_code, :category, :work_description, :password, :role])
    end
+
 end
