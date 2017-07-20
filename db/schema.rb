21a6fcd4973efab93cc1765808ded7c5c789e86e
aqueza2 <aqueza2@gmail.com>
2017-07-19 17:17:01 -0700

    #fix merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401155042) do
+ActiveRecord::Schema.define(version: 20170605004657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20170401155042) do
     t.string   "authentication_token",   limit: 30
     t.string   "role"
     t.string   "work_description"
+    t.string   "avatar"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
